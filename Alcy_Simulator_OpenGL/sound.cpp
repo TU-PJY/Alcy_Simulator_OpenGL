0e83a5f2aa70aee26905b50bbd6cdfd8c6ac89c2
TU-PJY <viska0319@icloud.com>
2024-02-26 19:30:43 +0900

    사운드 파일을 종류별로 분류

--- Alcy_Simulator_OpenGL/sound.cpp
+++ Alcy_Simulator_OpenGL/sound.cpp
@@ -18,29 +18,26 @@ bool escSoundPlayed;  // 중복 재생 방지
 void initFmod() {
 	ssystem->init(64, FMOD_INIT_NORMAL, extradriverdata);
 
-	ssystem->createSound("res//sound//button_click.wav", FMOD_DEFAULT, 0, &click);
-	ssystem->createSound("res//sound//button_down.wav", FMOD_DEFAULT, 0, &escDown);
-	ssystem->createSound("res//sound//button_up.wav", FMOD_DEFAULT, 0, &escUp);
-	ssystem->createSound("res//sound//scroll.wav", FMOD_DEFAULT, 0, &scroll);
-	ssystem->createSound("res//sound//menu_open.wav", FMOD_DEFAULT, 0, &menuOpen);
-	ssystem->createSound("res//sound//menu_close.wav", FMOD_DEFAULT, 0, &menuClose);
-	ssystem->createSound("res//sound//menu_click.wav", FMOD_DEFAULT, 0, &menuClick);
+	ssystem->createSound("res//sound//UI//button_click.wav", FMOD_DEFAULT, 0, &click);
+	ssystem->createSound("res//sound//UI//button_down.wav", FMOD_DEFAULT, 0, &escDown);
+	ssystem->createSound("res//sound//UI//button_up.wav", FMOD_DEFAULT, 0, &escUp);
+	ssystem->createSound("res//sound//UI//scroll.wav", FMOD_DEFAULT, 0, &scroll);
+	ssystem->createSound("res//sound//UI//menu_open.wav", FMOD_DEFAULT, 0, &menuOpen);
+	ssystem->createSound("res//sound//UI//menu_close.wav", FMOD_DEFAULT, 0, &menuClose);
+	ssystem->createSound("res//sound//UI//menu_click.wav", FMOD_DEFAULT, 0, &menuClick);
 
-	ssystem->createSound("res//sound//squeak1.mp3", FMOD_DEFAULT, 0, &squeak1);
-	ssystem->createSound("res//sound//squeak2.mp3", FMOD_DEFAULT, 0, &squeak2);
-	ssystem->createSound("res//sound//squeak3.mp3", FMOD_DEFAULT, 0, &squeak3);
-
-	ssystem->createSound("res//sound//breathe_in.wav", FMOD_DEFAULT, 0, &breatheIn);
-	ssystem->createSound("res//sound//breathe_out.wav", FMOD_DEFAULT, 0, &breatheOut);
-
-	ssystem->createSound("res//sound//touch.wav", FMOD_LOOP_NORMAL, 0, &touch);
-	ssystem->createSound("res//sound//tilt.wav", FMOD_DEFAULT, 0, &tilt);
+	ssystem->createSound("res//sound//alcy//squeak1.mp3", FMOD_DEFAULT, 0, &squeak1);
+	ssystem->createSound("res//sound//alcy//squeak2.mp3", FMOD_DEFAULT, 0, &squeak2);
+	ssystem->createSound("res//sound//alcy//squeak3.mp3", FMOD_DEFAULT, 0, &squeak3);
+	ssystem->createSound("res//sound//alcy//breathe_in.wav", FMOD_DEFAULT, 0, &breatheIn);
+	ssystem->createSound("res//sound//alcy//breathe_out.wav", FMOD_DEFAULT, 0, &breatheOut);
+	ssystem->createSound("res//sound//alcy//touch.wav", FMOD_LOOP_NORMAL, 0, &touch);
+	ssystem->createSound("res//sound//alcy//tilt.wav", FMOD_DEFAULT, 0, &tilt);
 
 	ssystem->createSound("res//sound//music//music1.mp3", FMOD_DEFAULT, 0, &music1);
 	ssystem->createSound("res//sound//music//music2.mp3", FMOD_DEFAULT, 0, &music2);
 	ssystem->createSound("res//sound//music//guitar1.mp3", FMOD_DEFAULT, 0, &music3);
 	ssystem->createSound("res//sound//music//guitar2.mp3", FMOD_DEFAULT, 0, &music4);
-
 	ssystem->createSound("res//sound//music//main_theme.mp3", FMOD_LOOP_NORMAL, 0, &mainTheme);
 
 	if(INTRO == 0)  // 인트로를 비활성화 했을 경우 메인 음악을 바로 킨다
