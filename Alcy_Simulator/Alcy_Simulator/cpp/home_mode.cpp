383308343af568114fba389c27b0d912d6b6cf03
TU-PJY <viska0319@icloud.com>
2024-05-05 16:06:02 +0900

    홈 모드 배경화면 추가

--- Alcy_Simulator/Alcy_Simulator/cpp/home_mode.cpp
+++ Alcy_Simulator/Alcy_Simulator/cpp/home_mode.cpp
@@ -1,8 +1,10 @@
 #include "../header/mode_header.h"
 #include "../object/main_object/Alcy_home.h"
 #include "../object/main_object/Cursor_home.h"
+#include "../object/main_object/BackGround_home.h"
 
 void home_mode() {
+	fw.add_object(new BackGround_home(background_layer, "background_home"), background_layer);
 	fw.add_object(new Alcy_home(alcy_layer, "alcy_home"), alcy_layer);
 	fw.add_object(new Cursor_home(ui_layer, "cursor_home"), ui_layer);
 }
---
+++ Alcy_Simulator/Alcy_Simulator/object/main_object/Background_home.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "../../header/image_text_util.h"
+#include "../../header/view.h"
+
+
+class BackGround_home : public FUNCTION{
+private:
+	GLuint VAO;
+	unsigned int tex;
+
+	int layer{};
+	std::string tag;
+
+public:
+	void update() {}
+
+	void render() {
+		init_transform();
+		
+		s_mat *= scale_image(15.0 * ratio, 15.0);
+		t_mat *= move_image(-cam.x / 2, -cam.y / 2);
+		draw_image(tex, VAO);
+	}
+
+	void check_collision() {}
+
+	void check_delete() {}
+
+
+	BackGround_home(int l, std::string str) {
+		layer = l;
+		tag = str;
+
+		set_canvas(VAO);
+		set_texture(tex, "res//background//default.png", 1440, 1440, 1);
+	}
+
+};
